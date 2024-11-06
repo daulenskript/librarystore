ec2dd01c3b0aa912966e46a5f9313f9615649f17
daulenskript <kazbekok7@gmail.com>
2024-11-07 02:03:51 +0600

    database added

---
+++ src/main/java/org/example/DatabaseConnection.java
@@ -0,0 +1,35 @@
+package org.example;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    private DatabaseConnection() {
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/library",
+                    "username",
+                    "password"
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
+
